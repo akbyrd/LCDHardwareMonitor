7283d8fad739dcebba063d83322854928aeab4c8
Adam Byrd <sycobob@gmail.com>
2015-03-23 00:30:12 -0500

    Super rough drag & drop drawable support. Don't judge me

--- LCD Hardware Monitor/src/Pages/DrawablesList.xaml.cs
+++ LCD Hardware Monitor/src/Pages/DrawablesList.xaml.cs
@@ -1,6 +1,8 @@
 ﻿namespace LCDHardwareMonitor.Pages
 {
+	using System.Windows;
 	using System.Windows.Controls;
+	using System.Windows.Input;
 
 	/// <summary>
 	/// Interaction logic for DrawablesList.xaml
@@ -11,5 +13,42 @@
 		{
 			InitializeComponent();
 		}
+
+		#region Drag and Drop Functionality
+
+		//TODO: Cleaner way to do this
+		private bool isClicked;
+
+		private void Drawable_MouseLeftButtonDown ( object sender, MouseEventArgs e )
+		{
+			var lvItem = sender as ListViewItem;
+			if ( lvItem != null && e.LeftButton == MouseButtonState.Pressed )
+			{
+				isClicked = true;
+			}
+		}
+
+		private void Drawable_MouseLeftButtonUp ( object sender, MouseEventArgs e )
+		{
+			var lvItem = sender as ListViewItem;
+			if ( lvItem != null )
+			{
+				isClicked = false;
+			}
+		}
+
+		private void Drawable_MouseMove ( object sender, MouseEventArgs e )
+		{
+			if ( isClicked )
+			{
+				var lvItem = sender as ListViewItem;
+				if ( lvItem != null && e.LeftButton == MouseButtonState.Pressed )
+				{
+					DragDrop.DoDragDrop(lvItem, lvItem.Content.GetType(), DragDropEffects.Copy);
+				}
+			}
+		}
+
+		#endregion
 	}
 }
---
+++ LCD Hardware Monitor/src/Pages/LCDPreview.xaml.cs
@@ -0,0 +1,92 @@
+﻿namespace LCDHardwareMonitor.Pages
+{
+	using System;
+	using System.ComponentModel;
+	using System.Runtime.CompilerServices;
+	using System.Windows;
+	using System.Windows.Controls;
+	using System.Windows.Media;
+	using System.Windows.Media.Effects;
+	using LCDHardwareMonitor.Drawables;
+
+	/// <summary>
+	/// Interaction logic for LCDPreview.xaml
+	/// </summary>
+	public partial class LCDPreview : UserControl, INotifyPropertyChanged
+	{
+		public  Effect HighlightEffect
+		{
+			get { return highlightEffect; }
+			private set { highlightEffect = value; RaisePropertyChangedEvent(); }
+		}
+		private Effect highlightEffect;
+
+		public LCDPreview ()
+		{
+			InitializeComponent();
+		}
+
+		#region Drag & Drop Functionality
+
+		private Type draggedDataType;
+		private DragDropEffects dropEffect;
+		private DropShadowEffect dropShadow = new DropShadowEffect() {
+			Color = new Color() { B = 255 }
+		};
+
+		//TODO: Cleanup
+		private void Canvas_DragEnter ( object sender, DragEventArgs e )
+		{
+			//var staticText = (StaticText) e.Data.GetData(typeof(StaticText));
+			object data = e.Data.GetData(e.Data.GetFormats()[0]);
+			draggedDataType = data as Type;
+
+			if ( draggedDataType != null && typeof(IDrawable).IsAssignableFrom(draggedDataType) )
+			{
+				dropEffect = DragDropEffects.Copy;
+				HighlightEffect = dropShadow;
+			}
+			else
+			{
+				dropEffect = DragDropEffects.None;
+				HighlightEffect = null;
+			}
+		}
+
+		private void Canvas_DragOver ( object sender, DragEventArgs e )
+		{
+			e.Effects = dropEffect;
+		}
+
+		private void Canvas_DragLeave ( object sender, DragEventArgs e )
+		{
+			dropEffect = DragDropEffects.None;
+			HighlightEffect = null;
+		}
+
+		private void Canvas_Drop ( object sender, DragEventArgs e )
+		{
+			dropEffect = DragDropEffects.None;
+			HighlightEffect = null;
+
+			var newDrawable = (IDrawable) Activator.CreateInstance(draggedDataType);
+		}
+
+		#endregion
+
+		#region INotifyPropertyChanged Implementation
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	
+		private void RaisePropertyChangedEvent ( [CallerMemberName] string propertyName = "" )
+		{
+			if ( PropertyChanged != null )
+			{
+				var args = new PropertyChangedEventArgs(propertyName);
+				PropertyChanged(this, args);
+			}
+		}
+
+		#endregion
+	}
+}
