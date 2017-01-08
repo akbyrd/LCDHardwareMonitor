e8f168212383b0e314e0f3d162b1474f13b57158
Adam Byrd <adam1.byrd@gmail.com>
2017-01-08 14:07:04 -0600

    Temp fix for DLL calling convention

--- LCDHardwareMonitor.Core/src/Renderers/D3D11Renderer.cs
+++ LCDHardwareMonitor.Core/src/Renderers/D3D11Renderer.cs
@@ -6,10 +6,11 @@
 	public static class D3D11Renderer
 	{
 		private const string DLLPath = @"Renderers\D3D11\LCDHardwareMonitor.Rendering.Direct3D11.dll";
+		private const CallingConvention CallConv = CallingConvention.Cdecl;
 
-		[DllImport(DLLPath)] public static extern bool   Initialize();
-		[DllImport(DLLPath)] public static extern bool   Render();
-		[DllImport(DLLPath)] public static extern void   Teardown();
-		[DllImport(DLLPath)] public static extern IntPtr GetD3D9RenderTexture();
+		[DllImport(DLLPath, CallingConvention = CallConv)] public static extern bool   Initialize(IntPtr hwnd);
+		[DllImport(DLLPath, CallingConvention = CallConv)] public static extern bool   Render();
+		[DllImport(DLLPath, CallingConvention = CallConv)] public static extern void   Teardown();
+		[DllImport(DLLPath, CallingConvention = CallConv)] public static extern IntPtr GetD3D9RenderSurface();
 	}
 }
