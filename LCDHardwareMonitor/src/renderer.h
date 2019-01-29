64445f1887241b8f55e9240409eaea3012e38950
Adam Byrd <adam1.byrd@gmail.com>
2019-01-28 22:43:59 -0800

    Missed file in previous commit

--- LCDHardwareMonitor/src/renderer.h
+++ LCDHardwareMonitor/src/renderer.h
@@ -41,9 +41,9 @@ struct DrawCall
 b32                   Renderer_Initialize                   (RendererState*, v2i renderSize);
 b32                   Renderer_RebuildSharedGeometryBuffers (RendererState*);
 void                  Renderer_Teardown                     (RendererState*);
-Mesh                  Renderer_CreateMesh                   (RendererState*, Slice<c8> name, Slice<Vertex> vertices, Slice<Index> indices);
-VertexShader          Renderer_LoadVertexShader             (RendererState*, Slice<c8> name, c8* path, Slice<VertexAttribute> attributes, Slice<u32> cBufSizes);
-PixelShader           Renderer_LoadPixelShader              (RendererState*, Slice<c8> name, c8* path, Slice<u32> cBufSizes);
+Mesh                  Renderer_CreateMesh                   (RendererState*, StringSlice name, Slice<Vertex> vertices, Slice<Index> indices);
+VertexShader          Renderer_LoadVertexShader             (RendererState*, StringSlice name, c8* path, Slice<VertexAttribute> attributes, Slice<u32> cBufSizes);
+PixelShader           Renderer_LoadPixelShader              (RendererState*, StringSlice name, c8* path, Slice<u32> cBufSizes);
 // TODO: Combine with CreateMesh, LoadVertexShader, and LoadPixelShader
 Material              Renderer_CreateMaterial               (RendererState*, Mesh mesh, VertexShader vs, PixelShader ps);
 ConstantBufferUpdate* Renderer_PushConstantBufferUpdate     (RendererState*);
