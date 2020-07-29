001274ed599817e220db956d9af85961c3f59742
Adam Byrd <adam1.byrd@gmail.com>
2020-07-28 21:53:06 -0700

    Implement LCD initialization

--- Hardware Communication/Hardware Communication/ILI9341.h
+++ Hardware Communication/Hardware Communication/ILI9341.h
@@ -16,12 +16,34 @@ namespace ILI9341
 
 	struct Command
 	{
-		static const u8 SoftwareReset    = 0x01;
-		static const u8 SleepIn          = 0x10;
-		static const u8 SleepOut         = 0x11;
-		static const u8 ColumnAddressSet = 0x2A;
-		static const u8 PageAddressSet   = 0x2B;
-		static const u8 MemoryWrite      = 0x2C;
+		static const u8 Nop                          = 0x00;
+		static const u8 SoftwareReset                = 0x01;
+		static const u8 SleepIn                      = 0x10;
+		static const u8 SleepOut                     = 0x11;
+		static const u8 GammaSet                     = 0x26;
+		static const u8 DisplayOff                   = 0x28;
+		static const u8 DisplayOn                    = 0x29;
+		static const u8 ColumnAddressSet             = 0x2A;
+		static const u8 PageAddressSet               = 0x2B;
+		static const u8 MemoryWrite                  = 0x2C;
+		static const u8 MemoryAccessControl          = 0x36;
+		static const u8 PixelFormatSet               = 0x3A;
+		static const u8 FrameRateControlNormal       = 0xB1;
+		static const u8 DisplayFunctionControl       = 0xB6;
+		static const u8 PowerControl1                = 0xC0;
+		static const u8 PowerControl2                = 0xC1;
+		static const u8 VCOMControl1                 = 0xC5;
+		static const u8 VCOMControl2                 = 0xC7;
+		static const u8 PowerControlA                = 0xCB;
+		static const u8 PowerControlB                = 0xCF;
+		static const u8 PositiveGammaCorrection      = 0xE0;
+		static const u8 NegativeGammaCorrection      = 0xE1;
+		static const u8 DriverTimingControlAInternal = 0xE8;
+		static const u8 DriverTimingControlAExternal = 0xE9;
+		static const u8 DriverTimingControlB         = 0xEA;
+		static const u8 PowerOnSequenceControl       = 0xED;
+		static const u8 Enable3Gamma                 = 0xF2;
+		static const u8 PumpRatioControl             = 0xF7;
 	};
 }
 
@@ -80,6 +102,8 @@ void ILI9341_Write(ILI9341::State* ili9341, u8 cmd, Args... bytes)
 	ILI9341_WriteData(ili9341, bytes...);
 }
 
+// TODO: Something about this is wrong. If this is the only command sent back to back runs result in
+// a non-empty read buffer
 // TODO: Change rects to be exclusive of max?
 void ILI9341_SetRect(ILI9341::State* ili9341, u16 x1, u16 y1, u16 x2, u16 y2, u16 color)
 {
@@ -93,7 +117,6 @@ void ILI9341_SetRect(ILI9341::State* ili9341, u16 x1, u16 y1, u16 x2, u16 y2, u1
 	//Assert(MADCTL == 0 && y <= 0x013F);
 	//Assert(MADCTL == 1 && y <= 0x00EF);
 	ILI9341_Write(ili9341, ILI9341::Command::PageAddressSet, Unpack2(y1), Unpack2(y2));
-
 
 	const u32 maxWrite = 0xFFFE;
 	u8 colorData[maxWrite];
@@ -167,230 +190,158 @@ void ILI9341_Wake(ILI9341::State* ili9341)
 	//nextCommandTime = sleepTime + 5ms
 }
 
-void DocumentationInit(ILI9341::State* ili9341)
+// TODO: Revisit this when displaying LHM frames.
+void ILI9341_MemoryAccessControl(ILI9341::State* ili9341)
 {
-	// NOTE: I'm not sure why write_data16 passes two bytes. The first is always zero and is not part
-	// of the parameters.
+	// NOTE: I have no idea why bits 3 and 6 have to be flipped, but every other piece of code I can
+	// find does the same, including an example program from the manufacturer of one of these
+	// screens. Despite the documentation, I guess the hardware defaults to top right and BGR?
 
-	auto delay        = [](DWORD ms) { Sleep(ms); };
-	auto write_cmd    = [&](u8 cmd) { ILI9341_WriteCmd(ili9341, cmd); };
-	auto write_data16 = [&](u8 byte1, u8 byte2) { ILI9341_WriteData(ili9341, byte2); };
+	u8 rowAddressOrder_TopToBottom        = 0 << 7;
+	u8 colAddressOrder_LeftToRight        = 1 << 6;
+	u8 rowColExchange_No                  = 0 << 5;
+	u8 berticalRefreshOrder_TopToBottom   = 0 << 4;
+	u8 rgbOrder_RGB                       = 1 << 3;
+	u8 horizontalRefershOrder_LeftToRight = 0 << 2;
 
-	write_cmd(0x01); // software reset
-	delay(5);
-
-	write_cmd(0x28); // display off
-
-	//---------------------------------------------------------
-
-	write_cmd(0xCF); // power control B
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x83);
-	write_data16(0x00, 0x30);
-
-	write_cmd(0xED); // power on sequence control
-	write_data16(0x00, 0x64);
-	write_data16(0x00, 0x03);
-	write_data16(0x00, 0x12);
-	write_data16(0x00, 0x81);
-
-	write_cmd(0xE8); // driver timing control A
-	write_data16(0x00, 0x85);
-	write_data16(0x00, 0x01);
-	write_data16(0x00, 0x79);
-
-	write_cmd(0xCB); // power control A
-	write_data16(0x00, 0x39);
-	write_data16(0x00, 0x2C);
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x34);
-	write_data16(0x00, 0x02);
-
-	write_cmd(0xF7); // pump ratio control
-	write_data16(0x00, 0x20);
-
-	write_cmd(0xEA); // driver timing control B
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x00);
-
-	//------------power control------------------------------
-
-	write_cmd(0xC0); // power control 1
-	write_data16(0x00, 0x26);
-
-	write_cmd(0xC1); // power control 2
-	write_data16(0x00, 0x11);
-
-	//--------------VCOM---------
-
-	write_cmd(0xC5); // vcom control 1
-	write_data16(0x00, 0x35);
-	write_data16(0x00, 0x3E);
-
-	write_cmd(0xC7); // vcom control 2
-	write_data16(0x00, 0xBE); // 0x94
-
-	//------------memory access control------------------------
-
-	write_cmd(0x36); // memory access control
-	write_data16(0x00, 0x48); // my, mx, mv, ml, BGR, mh, 0.0
-
-	write_cmd(0x3A); // pixel format set
-	write_data16(0x00, 0x55); // 16bit / pixel
-
-	//-----------------  frame  rate------------------------------
-
-	write_cmd(0xB1); // frame control (normal mode)
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x1B); // 70
-
-	//----------------Gamma---------------------------------
-
-	write_cmd(0xF2); // 3Gamma Function Disable
-	write_data16(0x00, 0x08);
-
-	write_cmd(0x26); // gamma set
-	write_data16(0x00, 0x01); // gamma set 4 gamma curve 01/02/04/08
-
-	write_cmd(0xE0); // positive gamma correction
-	write_data16(0x00, 0x1F);
-	write_data16(0x00, 0x1A);
-	write_data16(0x00, 0x18);
-	write_data16(0x00, 0x0A);
-	write_data16(0x00, 0x0F);
-	write_data16(0x00, 0x06);
-	write_data16(0x00, 0x45);
-	write_data16(0x00, 0x87);
-	write_data16(0x00, 0x32);
-	write_data16(0x00, 0x0A);
-	write_data16(0x00, 0x07);
-	write_data16(0x00, 0x02);
-	write_data16(0x00, 0x07);
-	write_data16(0x00, 0x05);
-	write_data16(0x00, 0x00);
-
-	write_cmd(0xE1); // negative gamma correction
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x25);
-	write_data16(0x00, 0x27);
-	write_data16(0x00, 0x05);
-	write_data16(0x00, 0x10);
-	write_data16(0x00, 0x09);
-	write_data16(0x00, 0x3A);
-	write_data16(0x00, 0x78);
-	write_data16(0x00, 0x4D);
-	write_data16(0x00, 0x05);
-	write_data16(0x00, 0x18);
-	write_data16(0x00, 0x0D);
-	write_data16(0x00, 0x38);
-	write_data16(0x00, 0x3A);
-	write_data16(0x00, 0x1F);
-
-	//--------------ddram ---------------------
-
-	write_cmd(0x2A); // column address set
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0xEF);
-
-	write_cmd(0x2B); // page address set
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x00);
-	write_data16(0x00, 0x01);
-	write_data16(0x00, 0x3F);
-
-	//write_cmd(0x34); // tearing effect line off
-	//write_cmd(0x35); // tearing effect line on
-
-	//write_cmd(0xB4); // display inversion control
-	//write_data16(0x00, 0x00);
-
-	write_cmd(0xB7); // entry mode set
-	write_data16(0x00, 0x07);
-
-	//-----------------display---------------------
-
-	write_cmd(0xB6); // display function control
-	write_data16(0x00, 0x0A);
-	write_data16(0x00, 0x82);
-	write_data16(0x00, 0x27);
-	write_data16(0x00, 0x00);
-
-	write_cmd(0x11); // sleep out
-	delay(100);
-	write_cmd(0x29); // display on
-	delay(100);
-	write_cmd(0x2C); // memory write
+	u8 mac = 0;
+	mac |= rowAddressOrder_TopToBottom;
+	mac |= colAddressOrder_LeftToRight;
+	mac |= rowColExchange_No;
+	mac |= berticalRefreshOrder_TopToBottom;
+	mac |= rgbOrder_RGB;
+	mac |= horizontalRefershOrder_LeftToRight;
+	ILI9341_Write(ili9341, ILI9341::Command::MemoryAccessControl, mac);
 }
 
-void PythonInit(ILI9341::State* ili9341)
+void ILI9341_PixelFormatSet(ILI9341::State* ili9341)
 {
-	// Pixel Format 0x55 = 0b'0101'0101 = 16bpp RGB and MCU
+	u8 bpp16 = 0b101;
+	u8 bpp18 = 0b110;
 
-	ILI9341_Write(ili9341, 0xEF, 0x03, 0x80, 0x02);             //* Can't find command!
-	ILI9341_Write(ili9341, 0xCF, 0x00, 0xC1, 0x30);             //* Power Control B                    values
-	ILI9341_Write(ili9341, 0xED, 0x64, 0x03, 0x12, 0x81);       // Power On Sequence Control
-	ILI9341_Write(ili9341, 0xE8, 0x85, 0x00, 0x78);             //* Driver Timing Control A            1 extra 0
-	ILI9341_Write(ili9341, 0xCB, 0x39, 0x2C, 0x00, 0x34, 0x02); // Power Control A
-	ILI9341_Write(ili9341, 0xF7, 0x20);                         // Pump Ratio Control
-	ILI9341_Write(ili9341, 0xEA, 0x00, 0x00);                   // Driver Control Timing B
-	ILI9341_Write(ili9341, 0xC0, 0x23);                         //* Power Control 1, VRH[5:0]          value
-	ILI9341_Write(ili9341, 0xC1, 0x10);                         //* Power Control 2, SAP[2:0], BT[3:0] value
-	ILI9341_Write(ili9341, 0xC5, 0x3E, 0x28);                   //* VCM Control 1                      values
-	ILI9341_Write(ili9341, 0xC7, 0x86);                         //* VCM Control 2                      value
-	ILI9341_Write(ili9341, 0x36, 0x48);                         // Memory Access Control
-	ILI9341_Write(ili9341, 0x3A, 0x55);                         // Pixel Format
-	ILI9341_Write(ili9341, 0xB1, 0x00, 0x18);                   // FRMCTR1                             values
-	ILI9341_Write(ili9341, 0xB6, 0x08, 0x82, 0x27);             //* Display Function Control           value  order
-	ILI9341_Write(ili9341, 0xF2, 0x00);                         //* 3Gamma Function Disable            value
-	ILI9341_Write(ili9341, 0x26, 0x01);                         // Gamma Curve Selected
-	ILI9341_Write(ili9341, 0xE0, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00); // Set Gamma
-	ILI9341_Write(ili9341, 0xE1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F); // Set Gamma
-	// column
-	// page
-	// entry mode set
-	ILI9341_WriteCmd(ili9341, 0x11);                            // Sleep Out
-	ILI9341_WriteCmd(ili9341, 0x29);                            // Display On
-	// memory write
+	u8 mcuShift = 0;
+	u8 rgbShift = 4;
+
+	u8 pf = 0;
+	pf |= bpp16 << mcuShift;
+	pf |= bpp16 << rgbShift;
+
+	ILI9341_Write(ili9341, ILI9341::Command::PixelFormatSet, pf);
 }
 
-void ArdnewInit(ILI9341::State* ili9341)
+void ILI9341_FrameRateControl_Normal(ILI9341::State* ili9341)
 {
-	// Hardware reset
-	ILI9341_WriteCmd(ili9341, 0x01);                            // software reset
-	Sleep(200);
-	ILI9341_Write(ili9341, 0xCB, 0x39, 0x2C, 0x00, 0x34, 0x02); // power control A
-	ILI9341_Write(ili9341, 0xCF, 0x00, 0xC1, 0x30);             // power control B
-	ILI9341_Write(ili9341, 0xE8, 0x85, 0x00, 0x78);             // driver timing control A
-	ILI9341_Write(ili9341, 0xEA, 0x00, 0x00);                   // driver timing control B
-	ILI9341_Write(ili9341, 0xED, 0x64, 0x03, 0x12, 0x81);       // power-on sequence control
-	ILI9341_Write(ili9341, 0xF7, 0x20);                         // pump ratio control
-	ILI9341_Write(ili9341, 0xC0, 0x23);                         // power control VRH[5:0]
-	ILI9341_Write(ili9341, 0xC1, 0x10);                         // power control SAP[2:0] BT[3:0]
-	ILI9341_Write(ili9341, 0xC5, 0x3E, 0x28);                   // VCM control
-	ILI9341_Write(ili9341, 0xC7, 0x86);                         // VCM control 2
-	ILI9341_Write(ili9341, 0x36, 0x48);                         // memory access control
-	ILI9341_Write(ili9341, 0x3A, 0x55);                         // pixel format
-	ILI9341_Write(ili9341, 0xB1, 0x00, 0x18);                   // frame ratio control, standard RGB color
-	ILI9341_Write(ili9341, 0xB6, 0x08, 0x82, 0x27);             // display function control
-	ILI9341_Write(ili9341, 0xF2, 0x00);                         // 3-Gamma function disable
-	ILI9341_Write(ili9341, 0x26, 0x01);                         // gamma curve selected
-	ILI9341_Write(ili9341, 0xE0, 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00); // positive gamma correction
-	ILI9341_Write(ili9341, 0xE1, 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F); // negative gamma correction
-	ILI9341_WriteCmd(ili9341, 0x11);                            // exit sleep
-	Sleep(120);
-	ILI9341_WriteCmd(ili9341, 0x29);                            // turn on display
-	//ILI9341_Write(ili9341, 0x36, []uint8{lcd.config.Rotate.MADCTL()); // MADCTL
+	u8 divisionRatio_1 = 0b00;
+	u8 divisionRatio_2 = 0b01;
+	u8 divisionRatio_4 = 0b10;
+	u8 divisionRatio_8 = 0b11;
+
+	u8 frameRate_119 = 0b10000;
+	u8 frameRate_112 = 0b10001;
+	u8 frameRate_106 = 0b10010;
+	u8 frameRate_100 = 0b10011;
+	u8 frameRate_95  = 0b10100;
+	u8 frameRate_90  = 0b10101;
+	u8 frameRate_86  = 0b10110;
+	u8 frameRate_83  = 0b10111;
+	u8 frameRate_79  = 0b11000;
+	u8 frameRate_76  = 0b11001;
+	u8 frameRate_73  = 0b11010;
+	u8 frameRate_70  = 0b11011;
+	u8 frameRate_68  = 0b11100;
+	u8 frameRate_65  = 0b11101;
+	u8 frameRate_63  = 0b11110;
+	u8 frameRate_61  = 0b11111;
+
+	u8 frc_divisionRatio = divisionRatio_1;
+	u8 frc_clocksPerLine = frameRate_119;
+	ILI9341_Write(ili9341, ILI9341::Command::FrameRateControlNormal, frc_divisionRatio, frc_clocksPerLine);
+}
+
+void ILI9341_DisplayFunctionControl(ILI9341::State* ili9341)
+{
+	// NOTE: Display Function Control defaults are fine.
+}
+
+void ILI9341_PowerControl(ILI9341::State* ili9341)
+{
+	// NOTE: Power Control 1 defaults are fine.
+	// NOTE: Power Control 2 defaults are fine.
+	// NOTE: Power Control A defaults are fine.
+
+	u8 pcb_p1_default = 0x00;
+	u8 pcb_p2_default = 0x81;
+	u8 pcb_p2_pceq_enable = 1 << 6;
+
+	u8 pcb_p1 = pcb_p1_default;
+	u8 pcb_p2 = pcb_p2_default | pcb_p2_pceq_enable;
+
+	ILI9341_Write(ili9341, ILI9341::Command::PowerControlB, pcb_p1, pcb_p2);
+	// NOTE: Leave parameter 3 at default
+	ILI9341_WriteCmd(ili9341, ILI9341::Command::Nop);
+}
+
+void ILI9341_VCOMControl(ILI9341::State* ili9341)
+{
+	// NOTE: VCOM Control 1 defaults are fine.
+	// NOTE: VCOM Control 2 defaults are fine.
+}
+
+void ILI9341_DriverTimingControl(ILI9341::State* ili9341)
+{
+	// NOTE: Driver Timing Control A (1) defaults are fine
+	// NOTE: Driver Timing Control A (2) isn't used by any example code
+
+	// NOTE: Just doing what everyone else does...
+	ILI9341_Write(ili9341, ILI9341::Command::DriverTimingControlB, 0x00, 0x00);
+}
+
+void ILI9341_PowerOnSequenceControl(ILI9341::State* ili9341)
+{
+	// NOTE: Just doing what everyone else does...
+	ILI9341_Write(ili9341, ILI9341::Command::PowerOnSequenceControl, 0x64, 0x03, 0x12, 0x81);
+}
+
+void ILI9341_SetGamma(ILI9341::State* ili9341)
+{
+	// NOTE: Enable 3 Gamma default is fine
+	// NOTE: Gamma Set default is fine
+
+	// NOTE: Just doing what everyone else does...
+	ILI9341_Write(ili9341, ILI9341::Command::PositiveGammaCorrection,
+		0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00);
+	ILI9341_Write(ili9341, ILI9341::Command::NegativeGammaCorrection,
+		0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F);
+}
+
+void ILI9341_PumpRatioControl(ILI9341::State* ili9341)
+{
+	u8 ratioControl_2x = 0b10 << 4;
+
+	u8 prc_p1 = 0;
+	prc_p1 |= ratioControl_2x;
+	ILI9341_Write(ili9341, ILI9341::Command::PumpRatioControl, prc_p1);
 }
 
 void ILI9341_Initialize(ILI9341::State* ili9341, FT232H::State* ft232h)
 {
 	ili9341->ft232h = ft232h;
 
-	//DocumentationInit(ili9341);
-	PythonInit(ili9341);
-	//ArdnewInit(ili9341);
+	// TODO: Want to do a software reset to set all config to default, but it causes a flicker.
+	ILI9341_Reset(ili9341);
+
+	ILI9341_MemoryAccessControl(ili9341);
+	ILI9341_PixelFormatSet(ili9341);
+	ILI9341_FrameRateControl_Normal(ili9341);
+	ILI9341_DisplayFunctionControl(ili9341);
+	ILI9341_PowerControl(ili9341);
+	ILI9341_VCOMControl(ili9341);
+	ILI9341_DriverTimingControl(ili9341);
+	ILI9341_PowerOnSequenceControl(ili9341);
+	ILI9341_PumpRatioControl(ili9341);
+	ILI9341_SetGamma(ili9341);
+	ILI9341_WriteCmd(ili9341, ILI9341::Command::SleepOut);
+	ILI9341_WriteCmd(ili9341, ILI9341::Command::DisplayOn);
 }
 
 void ILI9341_Finalize(ILI9341::State* ili9341) {}
