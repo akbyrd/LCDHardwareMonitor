048b5be0434ac5510277ff04b9568e76fb4a31bc
Adam Byrd <adam1.byrd@gmail.com>
2020-08-02 15:36:07 -0700

    Stash

--- LCDHardwareMonitor API/LHMMemory.hpp
+++ LCDHardwareMonitor API/LHMMemory.hpp
@@ -1,6 +1,89 @@
 #ifndef LHM_MEMORY
 #define LHM_MEMORY
 
+struct AllocationHeader
+{
+	void* alloc;
+	// TODO: Stack trace?
+};
+
+template<typename T>
+T*
+AlignUp(T* pointer)
+{
+	void* memory = pointer;
+	u32 mod = memory % alignof(T);
+	return (T*) (memory + (!!mod * (alignof(T) - mod)));
+}
+
+//struct HeapAllocator {};
+//struct StackAllocator {};
+struct FixedAllocator
+{
+	void* tail;
+	void* memory;
+	u32   capacity;
+
+	template<typename T>
+	T&
+	ValueFromHeader(AllocationHeader& header)
+	{
+		T* value = (T*) ((void*) &header + sizeof(AllocationHeader));
+		value = AlignUp(value);
+		return *value;
+	}
+
+	template<typename T>
+	AllocationHeader*
+	HeaderFromValue(T* value);
+
+	template<typename T>
+	T&
+	Alloc()
+	{
+		// TODO: Is dereferencing unowned memory without reading/writing allowed?
+		AllocationHeader& header = (AllocationHeader&) *tail;
+		T& value = ValueFromHeader(header);
+		tail = &value + sizeof(T);
+		Assert(tail < memory + capacity);
+		header.alloc = &value;
+		return value;
+	}
+
+	template<typename T>
+	T&
+	Alloc(u32 count)
+	{
+		Assert(count);
+		T& value = Alloc<T>();
+		tail += (count - 1) * sizeof(T);
+		Assert(tail < memory + capacity);
+		return value;
+	}
+
+	template<typename T>
+	T* Free(T& value);
+
+	void
+	Reserve(u32 bytes)
+	{
+		Assert(!memory);
+		memory   = malloc(bytes);
+		capacity = bytes;
+		tail     = memory;
+	}
+};
+
+//struct LoggingContext {};
+
+struct GlobalContext
+{
+	//HeapAllocator  heap           = {};
+	//StackAllocator stack          = {};
+	FixedAllocator loggingReserve = {};
+	//LoggingContext logging        = {};
+};
+
 void OutOfMemory()
 {
 	// TODO: Logging
